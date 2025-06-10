ae81779211ae3e7d02d8ef693b13075164308bc6
Iván Pelli <ivanpellitadeo@gmail.com>
2025-06-10 19:17:56 -0300

    Add files via upload

--- config.php
+++ config.php
@@ -12,6 +12,12 @@ require_once 'vendor/autoload.php';
 //Make object of Google API Client for call Google API
 $google_client = new Google_Client();
 
+//Set the OAuth 2.0 Client ID | Copiar "Aqui tu ID DE CLIENTE"
+$google_client->setClientSecret('1008053947217-xxx.apps.googleusercontent.com');
+
+//Set the OAuth 2.0 Client Secret key Aqui tu CLAVE
+$google_client->setClientSecret('GOCSPX-xxxx');
+
 //Set the OAuth 2.0 Redirect URI | URL AUTORIZADO
 $google_client->setRedirectUri('http://localhost/loginariza/accesocorrecto.php');
 
